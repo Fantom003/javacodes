87ae307eb81a8e81e9c08cb1125956060efe2948
fantom003 <bohramohit765@gmail.com>
2023-05-19 18:17:33 +0530

    19javascript

---
+++ index.js
@@ -0,0 +1,22 @@
+function add(){
+    let a= parseInt (prompt("Enter the no:"))
+    let b= parseInt (prompt("Enter the no"))
+    document.write(a+b ,"<br>",);
+}
+function sub(){
+    
+    let a=prompt("Enter the no:")
+    let b=prompt("Enter the no:")
+    document.write(a-b ,"<br>",);
+}
+function multiply(){
+    let a=prompt("Enter the no:")
+    let b=prompt("Enter the no:")
+    document.write(a*b ,"<br>",);
+}
+function divide(){
+    let a=prompt("Enter the no:")
+    let b=prompt("Enter the no:")
+    document.write(a/b ,"<br>",);
+}
+
